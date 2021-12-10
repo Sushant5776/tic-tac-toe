28667dcd57c6ab9e5da13d35e9309b9e71d048d7
Sushant Garudkar <garudkarsushant7@gmail.com>
2021-12-10 15:29:23 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="app">
+      Hare Krishna Hare Krishna Krishna Krishna Hare Hare Hare Ram Hare Ram Ram Ram Hare Hare
+    </div>
+  )
+}
+
+export default App
